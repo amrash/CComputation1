a7702a52897d549a5a9cb0c8e7d26eeb936a2aa7
Amreen <amreenashraf@gmail.com>
2018-10-01 22:11:17 -0400

    commit #1

--- sketch.js
+++ sketch.js
@@ -1,14 +1,17 @@
 var song;
 var slider;
 
-function preload(){
-    song = loadSound("sound.mp3")
-}
+
 
 function setup(){
     createCanvas(200,200);
+    song = loadSound("sound.mp3",loaded);
     slider = createSlider(0,1,0.5,0.01);
+}
+
+function loaded(){
     song.play();
+//playing song through call back method instead of preload method to get rid of "loading..." on the browser
 }
 
 function draw(){
