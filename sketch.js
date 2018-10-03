82de61a5fdd115d2b6f0c600e5895919ca4341b8
Amreen <amreenashraf@gmail.com>
2018-10-03 15:34:31 -0400

    commit #2

--- sketch.js
+++ sketch.js
@@ -1,20 +1,52 @@
 var song;
-var slider;
+//var slider;
+//var slaider has been commented out for now but sliderPan may be needed later on
+var button;
 
 
 
 function setup(){
-    createCanvas(200,200);
+    createCanvas(400,400);
     song = loadSound("sound.mp3",loaded);
-    slider = createSlider(0,1,0.5,0.01);
+    button = createButton("play");
+    button.mousePressed(togglePlaying);
+    //slider = createSlider(0,1,0,0.01); 
+    //initial slider made to control volume of the song played
+    //song.setVolume(0.5);
+    //sliderRate = createSlider(0,3,1,0.01);
+    //sliderPan = createSlider(0,1,0.5,0.01);
+    background(51);
+
+}
+function loaded (){
+	console.log("loaded");
 }
 
-function loaded(){
-    song.play();
-//playing song through call back method instead of preload method to get rid of "loading..." on the browser
+function togglePlaying(){
+	if(!song.isPlaying()){
+		song.play();
+		song.setVolume(0.5);
+		button.html("pause");
+	} else{
+		song.pause();
+		button.html("play");
+	}
+	
+    //song.play()
+    //song.setVolume(0.5);
+
 }
+//function loaded(){
+    //song.play();
+//playing song through call back method instead of preload method to get rid of "loading..." on the browser
+//}
 
 function draw(){
-    background(0);
-    song.setVolume(slider.value());
+    //background(random(225));
+    //line(0,0,200,200);
+    //line(200,0,0,200);
+    //strokeWeight(18);
+    //song.setVolume(slider.value());
+    //song.pan(sliderPan.value());
+    //song.Rate(sliderRate.value());
 }
